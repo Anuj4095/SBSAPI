6b8accfde5734379404a4ce0b363eab1f7aaa703
Anuj4095 <anuj08635@gmail.com>
2026-04-09 08:31:58 +0530

    changes in app

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const userRouts = require('./Routes/user')
 const contactRouts = require('./Routes/contact')
 const mongoose=require('mongoose')
 
-const connectWithDatabase = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log('connected with database')
-    }
-    catch(err){
-        console.log('something is wrong')
-        console.log(err)
-    }
+// const connectWithDatabase = async()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGODB_URL)
+//         console.log('connected with database')
+//     }
+//     catch(err){
+//         console.log('something is wrong')
+//         console.log(err)
+//     }
 
-}
-connectWithDatabase()
+// }
+// connectWithDatabase()
 
 app.use('/user',userRouts)
 app.use('/contact',contactRouts)
